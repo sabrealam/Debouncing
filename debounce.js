8eb44b1c5695f1d03733efa10179b3a2447b7d77
Sabre Alam <142332803+sabrealam@users.noreply.github.com>
2024-01-04 00:21:34 +0545

    Change Theme

--- debounce.js
+++ debounce.js
@@ -3,20 +3,18 @@ let suggest = document.querySelector('#suggest');
 let suggest1 = document.querySelector('#suggest1');
 let data = [];
 window.addEventListener('load', function(){
-    alert('Search For A Movies And Meals \nClick On The Suggestion To Get Redirect To The Details Page')
+    alert('Search For A Movies And Meals \nClick On The Suggestion To Get Redirect To The More Details Page')
 })
 async function makeAPI(){ 
     try {
-        let res =await fetch(`https://www.omdbapi.com/?t=${movienames.value}&apikey=92a49ce9`);
+        let res =await fetch(`https://www.omdbapi.com/?s=${movienames.value}&apikey=92a49ce9`);
           data = await res.json();  
         if(data.Response === 'True' ){
-            suggest.style.display = 'block';
-            createHyperLink()
-            count++;
-            document.querySelector('#one').innerHTML = `You Have Made Total ${count} Requests For This Movie Search`;
-            document.querySelector('.p1').innerHTML +=  "✔️";
+            suggest.style.display = 'block'; 
+            createHyperLink(data.Search) 
         }else{
-            alert('We Did Not Find Any Movie With This Keyword')
+            document.querySelector('#render').innerHTML = null;
+            document.querySelector('#suggest').innerHTML = "Movie Not Found";
         } 
     } catch (err) {
         console.log(err);
@@ -25,20 +23,37 @@ async function makeAPI(){
 }
 
 
-function createHyperLink(){ 
-    let a = document.createElement('a');
+function createHyperLink(moviesList){
+    console.log(moviesList); 
+    document.querySelector('#render').innerHTML = null;
+    moviesList.forEach(ele=>{
+       let a = document.createElement('a');
         let br = document.createElement('br');
         a.href = `https://www.omdbapi.com/?t=${movienames.value}&apikey=92a49ce9`;
-        a.innerHTML = data.Title;
+        a.innerHTML = ele.Title;
         a.target = "_blank" ;
         suggest.append(a,br)
-      
-}
+        let render = document.querySelector('#render');
 
-function check(){
-    let suggest1 = document.querySelectorAll('#suggest > a');
-    return suggest1;
-}
+        let card = document.createElement('div');
+
+        let poster = document.createElement('img');
+        poster.src = ele.Poster;
+
+        let h3 = document.createElement('h3');
+        h3.innerHTML = ele.Title;
+
+        let h5 = document.createElement('h5');
+        h5.innerHTML = ele.Type;
+
+        let p = document.createElement('p');
+        p.innerHTML = ele.Year;
+        
+        card.append(poster,h3,h5,p);
+        render.append(card);
+         
+    })  
+} 
  
 
 let clr;
@@ -60,18 +75,17 @@ async function makeAPI1(){
             let mealname = document.querySelector('#mealname'); 
             let res =await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealname.value}`);
             data1 = await res.json(); 
-            if(data1.meals.length >= 1){
-                suggest1.style.display = 'block';
-                ct++;
-                createHyperLink1()
-                document.querySelector('#two').innerHTML = `You Have Made Total ${ct} Requests For This Searching`;
-                document.querySelector('.p2').innerHTML +=  `✔️`;
-            }else{
-                alert('We Did Not Find Anything With This Keyword')
+             
+            if(data1.meals !== 'null'){
+                createHyperLink1(data1.meals) 
+            } else if(data1.meals == 'null') {
+                document.querySelector('#render').innerHTML = null;
+                document.querySelector('#suggest1').innerHTML = "Meal Not Found";
             }
-            // console.log(data1);
+             
         } catch (error) {
-            console.log(error);
+            document.querySelector('#render').innerHTML = null;
+            document.querySelector('#suggest1').innerHTML = "Meal Not Found"; 
         }
 }  
 let clr1;
@@ -89,30 +103,35 @@ function debounce1(callback,deley){
 }
  
 
-function createHyperLink1(){ 
-    data1.meals.forEach(e=>{
+function createHyperLink1(mealList){  
+    document.querySelector('#render').innerHTML = null;
+   mealList.forEach(e=>{
         let a = document.createElement('a');
         let br = document.createElement('br');
         a.href = `https://www.themealdb.com/api/json/v1/1/search.php?s=${e.strMeal}`;
         a.innerHTML = e.strMeal;
         a.target = "_blank" ;
         suggest1.append(a,br) 
+        let render = document.querySelector('#render');
+
+        let card = document.createElement('div');
+
+        let poster = document.createElement('img');
+        poster.src = e.strMealThumb;
+
+        let h3 = document.createElement('h3');
+        h3.innerHTML = e.strMeal;
+
+        let h5 = document.createElement('h5'); 
+        
+        card.append(poster,h3,h5);
+        render.append(card);
       })
     
       
 }
 
-
-
-
-
-
-
-
-
-
-
-
+ 
 
 window.addEventListener('keyup',function(event){   
     if(event.key === 'Enter'){
